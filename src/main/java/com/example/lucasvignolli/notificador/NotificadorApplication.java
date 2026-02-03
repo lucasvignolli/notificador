e3db464925bea86ce76fb4363885365e3b966c26
Lucas Vignolli <lucas.vignolli@gmail.com>
2026-02-03 16:50:19 -0300

    commit inicial

---
+++ src/main/java/com/example/lucasvignolli/notificador/NotificadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lucasvignolli.notificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lucasvignolli/notificador/NotificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lucasvignolli.notificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
